c053959b78b79bbb3e861787b6b99eff8a1b6570
Alain Trottier <alain.trottier15@gmail.com>
2020-11-20 11:09:04 -0500

    adding project page

--- server.js
+++ server.js
@@ -50,4 +50,10 @@ app.get('/bad', (req, res) => {
   })
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  })
+});
+
 app.listen(port, ()=>{console.log('Server is up on port 3000')});
